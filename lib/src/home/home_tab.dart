2d02bcad193cb3ba4c59ada9011a24afc6f8a6c8
arielguareschi <arielguareschi@gmail.com>
2023-06-02 16:06:07 -0300

    tela home

---
+++ lib/src/home/home_tab.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:mercadinho/src/config/custom_colors.dart';
+import 'package:badges/badges.dart' as packageBadge;
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBar
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text.rich(
+          TextSpan(
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+            children: [
+              TextSpan(
+                text: "Green",
+                style: TextStyle(
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+              TextSpan(
+                text: "grocer",
+                style: TextStyle(
+                  color: CustomColors.customConstrastColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15,
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: packageBadge.Badge(
+                badgeStyle: packageBadge.BadgeStyle(
+                  badgeColor: CustomColors.customConstrastColor,
+                ),
+                badgeContent: const Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Campo de Pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  isDense: true,
+                  hintText: 'Pesquise aqui...',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade400,
+                    fontSize: 14,
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: CustomColors.customConstrastColor,
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(60),
+                      borderSide: const BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ))),
+            ),
+          )
+          // Categorias
+
+          // Grid
+        ],
+      ),
+    );
+  }
+}
