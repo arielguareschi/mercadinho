77b9dd8575c3c4c820d5438fc3e8e37fd0a2c575
Ariel Guareschi <arielguareschi@gmail.com>
2023-06-24 09:46:15 -0300

    add get

---
+++ lib/src/pages_routes/app_pages.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+abstract class AppPages {
+  static final pages = <GetPage>[
+    GetPage(
+      name: "/splash",
+      page: () => const SplashScreen(),
+    ),
+  ];
+}
